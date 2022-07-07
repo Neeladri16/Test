396c3d87138ab1594247b471eb8a89b150153c5e
monika-sahu-github <monika.sahu@nxgsols.com>
2022-07-07 16:22:19 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,33 @@
+function add()
+{   
+    let num1,num2,res;
+    num1=Number(document.calculator.Number1.value);
+    num2=Number(document.calculator.Number2.value);
+    res=num1+num2;
+    document.getElementById("form").innerHTML="The result calculated is"+ res +".";
+   
+}
+function sub()
+{
+    var num1,num2,res;
+    num1=Number(document.calculator.Number1.value);
+    num2=Number(document.calculator.Number2.value);
+    res=num1-num2;
+    document.getElementById("form").innerHTML="The result calculated is"+ res +".";
+}
+function mult()
+{
+    var num1,num2,res;
+    num1=Number(document.calculator.Number1.value);
+    num2=Number(document.calculator.Number2.value);
+    res=num1*num2;
+    document.getElementById("form").innerHTML="The result calculated is"+ res +".";
+}
+function div()
+{
+    var num1,num2,res;
+    num1=Number(document.calculator.Number1.value);
+    num2=Number(document.calculator.Number2.value);
+    res=num1/num2;
+    document.getElementById("form").innerHTML="The result calculated is"+ res +".";
+}
