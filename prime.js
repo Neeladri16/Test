d0193a31978bd8c472bab8b45271c201595458e6
Neeladri Mukherjee <mukharjeeneel2000@gmail.com>
2022-07-11 11:42:38 +0530

    validation

--- prime.js
+++ prime.js
@@ -1,40 +1,81 @@
 function number(val)
 {  if(val=='odd')
-   {
-    var first=document.getElementById("first").value;
-    var last=document.getElementById("last").value;
-    var num="<br>Odd Numbers:<br>";
+   {   var correct_way=/^[0-9]+$/;
+       var first=document.getElementById("first").value;
+       var last=document.getElementById("last").value;
+          if(first.match(correct_way) && last.match(correct_way))
+              true;
+          else{
+              document.getElementById("message").innerHTML="**Only numbers are allowed";
+               return false;
+              }
+             if(first.length>last.length || last.length<first.length){
+        document.getElementById("message").innerHTML="** Range can't be calculated";
+        return false;
+    }
+        if(first=="" || last==""){
+         document.getElementById("message").innerHTML="** Please enter both numbers!";
+         return false;
+    }
+   if(first.length<0){
+     document.getElementById("message").innerHTML="**First number cant be negative!";
+     return false;
+   }
+   
+    var num='';
+    arr=[];
     for(i=first;i<=last;i++)
-    {
+    { 
         if(i%2!=0)
         {
-            num += i + ",";
-            number.push(num);
-            const num=[];
-            let length=num.length;
+            num = i;
+          arr.push(num);
         }
     }
-    document.getElementById("result").innerHTML=length;
+    document.getElementById("result").innerHTML="<br>Odd Numbers:<br>"+arr;
+    document.getElementById("odd").innerHTML="<br>No.of Odd numbers:<br>"+arr.length;
      }
     else if(val=='even')
     {
-        var first=document.getElementById("first").value;
-        var last=document.getElementById("last").value;
-        var num="<br>Even Numbers:<br>";
+           var correct_way=/^[0-9]+$/;
+    var first=document.getElementById("first").value;
+     var last=document.getElementById("last").value;
+   if(first.match(correct_way) && last.match(correct_way))
+      true;
+      else{
+         document.getElementById("message").innerHTML="**Only numbers are allowed";
+          return false;
+      }
+        
+
+        var num='';
+        arr=[];
         for(i=first;i<=last;i++)
         {
             if(i%2==0)
             {
-                num += i + ",";
+                num = i;
+                arr.push(num);
             }
         }
-        document.getElementById("result").innerHTML=num;
+        document.getElementById("result").innerHTML="<br>Even Numbers:<br>"+arr;
+        document.getElementById("odd").innerHTML="<br>No. of Even numbers:<br>"+arr.length;
          }
     else if(val=='prime')
-        { var first=document.getElementById("first").value;
-          var last=document.getElementById("last").value;
-          var num="<br>Prime Numbers:<br>";
+        {  var correct_way=/^[0-9]+$/;
+           var first=document.getElementById("first").value;
+           var last=document.getElementById("last").value;
+   if(first.match(correct_way) && last.match(correct_way))
+      true;
+      else{
+         document.getElementById("message").innerHTML="**Only numbers are allowed";
+          return false;
+      }
+
+
+          var num='';
          var temp=0;
+         arr=[];
           for(i=first;i<=last;i++)
             {
                 for(j=2;j<i;j++)
@@ -50,8 +91,10 @@ function number(val)
                 }
                 if(temp)
                 {
-                    num+=i+",";
-                    document.getElementById("result").innerHTML=num;
+                    num=i;
+                    arr.push(num);
+                    document.getElementById("result").innerHTML="<br>Odd Numbers:<br>"+arr;
+                    document.getElementById("odd").innerHTML="<br>No. of prime numbers:<br>"+arr.length;
                 }
             }
 
