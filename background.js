a1fdc8c04d5afc6e308a20b45866666f2de7527f
monika-sahu-github <monika.sahu@nxgsols.com>
2022-07-11 15:07:13 +0530

    js_colors

---
+++ background.js
@@ -0,0 +1,17 @@
+function myFunction(val){
+    if(val=='red'){
+        document.getElementById("color").style.backgroundColor="red";
+    }
+    else if(val=='blue'){
+            document.getElementById("color").style.backgroundColor="blue";
+        }
+        else if(val=='yellow'){
+            document.getElementById("color").style.backgroundColor="yellow";
+        }
+        else if(val=='green'){
+            document.getElementById("color").style.backgroundColor="green";
+        }
+        else if(val=='pink'){
+            document.getElementById("color").style.backgroundColor="pink";
+        }
+    }
